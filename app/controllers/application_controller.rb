8c404ae44bb801ad2bf80cdebb586e8696c12e4b
kiizerd <ajsewell13@gmail.com>
2021-11-09 16:00:00 -0600

    add method for dark mode

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_theme
+
+  def set_theme
+    if params[:theme].present?
+      theme = params[:theme].to_sym
+      cookies[:theme] = theme
+      redirect_to(request.referrer || root_path)
+    end
+  end
 
   protected
 
